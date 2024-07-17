f4d38453ac5b61ff96ec553a7292e7a5f9bd1f23
Tayrika <rika13tay@gmail.com>
2024-07-17 14:07:35 +0900

    changed config setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
+<<<<<<< HEAD
   config.assets.compile = false
+=======
+  config.assets.compile = true
+>>>>>>> e6f082f (changed config setting)
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
